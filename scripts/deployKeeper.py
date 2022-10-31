2091e2f5666a0dbec5b0d9213e048f4ed633ad36
cypherp0NK <102962217+cypherp0NK@users.noreply.github.com>
2022-10-31 15:33:21 +0100

    update scripts

--- scripts/deployKeeper.py
+++ scripts/deployKeeper.py
@@ -5,11 +5,10 @@ interval = 43200
 vault = "0x9e6A7B0532184c7Eb7DC536E1cA7c7606B299a8f"
 baseThreshold = 100 #2
 limitThreshold = 20 #1
-maxTwapDeviation = 100
-twapDuration = 60
+
 
 def main():
     account = accounts.load("cypherp0NK")
-    sharpe_keeper = account.deploy(SharpeKeeper, interval, vault, baseThreshold, limitThreshold,maxTwapDeviation,twapDuration,publish_source=True) 
+    sharpe_keeper = account.deploy(SharpeKeeper, interval, vault, baseThreshold, limitThreshold,publish_source=True) 
     print (sharpe_keeper)
     
